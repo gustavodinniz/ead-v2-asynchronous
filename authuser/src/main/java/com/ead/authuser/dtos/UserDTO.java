1b4938a70bafe5932a9c4e30bcb7fda40fa03802
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-04-27 19:46:37 -0300

    create consumer and receive event state transfer

--- authuser/src/main/java/com/ead/authuser/dtos/UserDTO.java
+++ authuser/src/main/java/com/ead/authuser/dtos/UserDTO.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
 import lombok.Singular;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -58,6 +59,7 @@ public class UserDTO {
     @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String phoneNumber;
 
+    @CPF(groups = {UserView.RegistrationPost.class, UserView.UserPut.class})
     @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String cpf;
 
