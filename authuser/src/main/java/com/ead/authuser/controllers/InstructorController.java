7395e08666fc0ab0dcec463f480ecb28a47ff7c5
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-05-08 20:13:36 -0300

    authorization roles for instructor

--- authuser/src/main/java/com/ead/authuser/controllers/InstructorController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/InstructorController.java
@@ -1,8 +1,11 @@
 package com.ead.authuser.controllers;
 
 import com.ead.authuser.dtos.InstructorDTO;
+import com.ead.authuser.enums.RoleType;
 import com.ead.authuser.enums.UserType;
+import com.ead.authuser.models.RoleModel;
 import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.RoleService;
 import com.ead.authuser.services.UserService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,15 +27,21 @@ public class InstructorController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    RoleService roleService;
+
     @PostMapping("/subscription")
     public ResponseEntity<Object> saveSubscriptionInstructor(@RequestBody @Valid InstructorDTO instructorDTO) {
         Optional<UserModel> userModelOptional = userService.findById(instructorDTO.getUserId());
         if (userModelOptional.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
         } else {
+            RoleModel roleModel = roleService.findByRoleName(RoleType.ROLE_INSTRUCTOR)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
             var userModel = userModelOptional.get();
             userModel.setUserType(UserType.INSTRUCTOR);
             userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+            userModel.getRoles().add(roleModel);
             userService.updateUser(userModel);
             return ResponseEntity.status(HttpStatus.OK).body(userModel);
         }
